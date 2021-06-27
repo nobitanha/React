9948ec83258bf0fd784179f747e095ebdfcbc21c
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-06-27 17:12:29 +0900

    Props and State demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-
+  const onClickButton = () => {
+    setNum1(num + 1);
+  };
+  const [num, setNum1] = useState(0);
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>こんにちは！ </h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink" message="お元気です。" />
 
-      <button onClick={onClickButton}>Button</button>
+      <button onClick={onClickButton}>Count up</button>
+      <p> {num} </p>
     </React.Fragment>
   );
 };
